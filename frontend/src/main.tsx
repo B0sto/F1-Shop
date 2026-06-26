cfbb733f1072979785a897f9b35d8dcfc03ac9b6
B0sto <bostoganashvilinika2019@gmail.com>
2026-06-26 20:42:55 +0400

    added toaster and changed scrollbar styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
 import { createAppRouter } from './router.tsx';
 import AuthInitializer from './components/auth/AuthInitializer.tsx';
+import { Toaster } from "sonner"
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,6 +23,17 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <AuthInitializer>
         <RouterProvider router={router} />
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            style: {
+              background: "#110D0D",
+              color: "#ffffff",
+              border: "1px solid #F90301",
+              fontFamily: "Akshar, serif",
+            },
+          }}
+        />
       </AuthInitializer>
     </QueryClientProvider>
   </StrictMode>,
